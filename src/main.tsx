4fe2568742c8e06572e4a72c16e296d758922966
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-05 11:31:33 -0600

    Installed Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
-import 'bootsr
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
