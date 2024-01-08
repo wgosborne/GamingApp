d0a89926061591c3c23a025a38235e18f10cd96a
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-07 18:36:50 -0600

    Create a custom hook to fetch the games

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         <GameGrid />
       </GridItem>
     </Grid>
