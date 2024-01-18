f8a87593d1a412a0df6b02f697c8b2408ab9fd69
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-18 09:24:05 -0600

    created generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import useGenres from '../hooks/useGenres'
+import React from "react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import useData from "../hooks/useData";
 
 const GenreList = () => {
-    const { genres } = useGenres();
+  const { data } = useGenres();
   return (
     <ul>
-        {genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+      {data.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default GenreList
+export default GenreList;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,45 +1,12 @@
-import { CanceledError } from "axios";
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-import { Game } from "./useGames";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
     id: number;
     name: string;
 }
 
-interface FetchGenresResponse {
-    count: number;
-    results: Genre[];
-}
 
-const useGenres = () => {
-    const [genres, setGenres] = useState<Genre[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
-  
-    useEffect(() => {
-        const controller = new AbortController();
+const useGenres = () => useData<Genre>('/genres');
 
-        setLoading(true);
-
-        apiClient
-            .get<FetchGenresResponse>("/genres", { signal: controller.signal})
-            .then((res) => {
-                setGenres(res.data.results);
-                setLoading(false); //this would usually be done in finally but finally doesnt work in TypeScript mode
-            })
-            .catch((err) => {
-                if (err instanceof CanceledError) return;
-                setError(err.message);
-                setLoading(false); //this would usually be done in finally but finally doesnt work in TypeScript mode
-            });
-
-        return () => controller.abort();
-
-    }, []);
-
-    return { genres, error, isLoading };
-}
 
 export default useGenres;
