ae14c509effe75f1e63a29a52e0d7940c1291751
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-18 09:57:27 -0600

    highlights the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCroppedImageURL from "../services/image-url";
 
 interface Props {
     onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if(error) return null;
@@ -23,7 +24,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
               borderRadius={8}
               src={getCroppedImageURL(genre.image_background)}
             />
-            <Button onClick={() => {onSelectGenre(genre)}} fontSize={"lg"} variant={'link'}>{genre.name}</Button>
+            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => {onSelectGenre(genre)}} fontSize={"lg"} variant={'link'}>{genre.name}</Button>
           </HStack>
         </ListItem>
       ))}
