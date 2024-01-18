cc32986558af02b8eb737311b2437ad7ad2fd6c6
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-18 09:37:30 -0600

    shows a spinner while fetching genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ function App() {
         base: `"nav" "main"`, //phone size
         lg: `"nav nav" "aside main"`, //desktop, wider than 1024px
       }}
+      templateColumns={{
+        base: '1fr', //one fraction, it will stretch to take up available space
+        lg: '200px 1fr'
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,16 +1,16 @@
-import { Box } from '@chakra-ui/react'
-import { ReactNode } from 'react'
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode
+  children: ReactNode;
 }
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow={'hidden'} width={'300px'}>
-        {children}
+    <Box borderRadius={10} overflow={"hidden"}>
+      {children}
     </Box>
-  )
-}
+  );
+};
 
-export default GameCardContainer
+export default GameCardContainer;
