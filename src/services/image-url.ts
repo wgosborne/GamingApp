64a4e60b444a033ddf40ab7b1c6df657c25566c5
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-17 22:16:15 -0600

    got optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,10 @@
+
+const getCroppedImageURL = (url: string) => {
+    const target = 'media/';
+    const index = url.indexOf(target) + target.length;
+
+    return url.slice(0, index) + 'crop/600/400/' + url.slice(index); //adding the cropping dimensions
+
+}
+
+export default getCroppedImageURL;
