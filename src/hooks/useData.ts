8b0e0bb4ba011850145eedcd61721cd61315d8ae
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-01-18 09:54:57 -0600

    filtered games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,4 +1,4 @@
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 
@@ -8,7 +8,7 @@ interface FetchResponse<T>{
     results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig,  deps?: any[]) => {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
     const [isLoading, setLoading] = useState(false);
@@ -19,7 +19,7 @@ const useData = <T>(endpoint: string) => {
         setLoading(true);
 
         apiClient
-            .get<FetchResponse<T>>(endpoint, { signal: controller.signal})
+            .get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
             .then((res) => {
                 setData(res.data.results);
                 setLoading(false); //this would usually be done in finally but finally doesnt work in TypeScript mode
@@ -32,7 +32,7 @@ const useData = <T>(endpoint: string) => {
 
         return () => controller.abort();
 
-    }, []);
+    }, deps ? [...deps] : []);
 
     return { data, error, isLoading };
 }
